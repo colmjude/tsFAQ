a91139a58b391d4aaead4d94e9aaa072d85d9271
Colm Britton <colmjude@gmail.com>
2013-10-15 16:55:59 +0100

    Update so minimum features work and there is a basic presentation

--- assets/faq.js
+++ assets/faq.js
@@ -14,10 +14,10 @@
 	$.getJSON("http://faq.tiddlyspace.com/tiddlers.json?select=tag:FAQ&fat=1&render=1", function(resp) {
 		if(resp) {
 			_.each(resp, function(item, index, remaining) {
-				if( _.contains(item.tags, "tiddlyspace") ) {
+				if( _.contains(item.tags, "TiddlySpace") ) {
 					ts_faqs.push( item );
 				}
-				if( _.contains(item.tags, "tiddlywiki") ) {
+				if( _.contains(item.tags, "TiddlyWiki") ) {
 					tw_faqs.push( item );
 				}
 			});
@@ -56,7 +56,7 @@
 
 		function renderBothSets() {
 			$main.append(generateFAQSection('tiddlyspace'));
-			$main.append(generateFAQSection('tiddlywiki'));
+            $main.append(generateFAQSection('tiddlywiki'));
 		}
 
 		if(fetched) {
@@ -69,7 +69,9 @@
 
 		$main.on("click", "dt", function(e) {
 			var $this = $(this);
-			$this.siblings().toggle().toggleClass('hidden');
+			$this
+			    .parent('dl')
+			        .toggleClass('closed');
 		});
 
 		
