e25167a5d489dd4294502fda16a73bbe538c4dbd
Colm Britton <colmjude@gmail.com>
2013-10-29 09:47:47 +0000

    Restructure js code.
    Ready for making more generic

--- assets/faq.js
+++ assets/faq.js
@@ -1,27 +1,40 @@
-;(function($) {
+;(function(exports, $) {
 	"use strict";
-	var fetched = false,
-        userIsMember = (tiddlyweb.status.space.recipe.match(/_private$/)) ? true : false,
-		ts_faqs = [],
-		tw_faqs = [],
-		faqtmpl;
 
-	$.getJSON("http://faq.tiddlyspace.com/tiddlers.json?select=tag:FAQ&fat=1&render=1", function(resp) {
-		if(resp) {
-			_.each(resp, function(item, index, remaining) {
-				if( _.contains(item.tags, "TiddlySpace") ) {
-					ts_faqs.push( item );
-				}
-				if( _.contains(item.tags, "TiddlyWiki") ) {
-					tw_faqs.push( item );
-				}
-			});
-			fetched = true;
-			$.event.trigger("fetched");
-		}
-	});
+    function FAQs() {
+        this.fetched = false;
+        this.faqtmpl = undefined;
+        this.ts_faqs = [];
+        this.tw_faqs = [];
+    }
 
-    function createEditLink(tidTitle) {
+    FAQs.prototype.init = function(tiddlyweb) {
+        this.userIsMember = (tiddlyweb.status.space.recipe.match(/_private$/)) ? true : false;
+        this.faqtmpl = Handlebars.compile($("#faq-template").html());
+        this.$main = $("#main");
+        this.bindUIEvents();
+        this.fetchFAQs();
+    };
+
+    FAQs.prototype.fetchFAQs = function() {
+        var context = this;
+        $.getJSON("http://faq.tiddlyspace.com/tiddlers.json?select=tag:FAQ&fat=1&render=1", function(resp) {
+            if(resp) {
+                _.each(resp, function(item, index, remaining) {
+                    if( _.contains(item.tags, "TiddlySpace") ) {
+                        context.ts_faqs.push( item );
+                    }
+                    if( _.contains(item.tags, "TiddlyWiki") ) {
+                        context.tw_faqs.push( item );
+                    }
+                });
+                context.fetched = true;
+                $.event.trigger("fetched");
+            }
+        });
+    };
+
+    FAQs.prototype.createEditLink = function(tidTitle) {
         var twS = tiddlyweb.status,
             twServer = twS.server_host;
         if( twS.username === "GUEST" ) {
@@ -30,61 +43,54 @@
             return twServer.scheme + "://" + twS.space.name + "." + 
                         twServer.host + "/edit#" + tidTitle;
         }
-    }
+    };
 
-	function generateFAQSection(type) {
-		var faqList = (type && type === "tiddlywiki") ? tw_faqs : ts_faqs;
+    FAQs.prototype.generateFAQSection = function(type) {
+        var context = this;
+        var faqList = (type && type === "tiddlywiki") ? context.tw_faqs : context.ts_faqs;
         var header = $("<header></header>").append( $("<h2></h2>", {"text":type, "class": "open"}) ),
             section = $("<section></section>")
                         .attr("id", type)
                         .addClass("open")
                         .append( header ),
-			faqwrap = $("<article></article>").appendTo(section);
+            faqwrap = $("<article></article>").appendTo(section);
 
-		_.each(faqList, function(faq, index, list) {
-			var data = {
-				title: faq.title,
-				href: createEditLink( faq.title ),
-				userIsMember: userIsMember
-			};
+        _.each(faqList, function(faq, index, list) {
+            var data = {
+                title: faq.title,
+                href: context.createEditLink( faq.title ),
+                userIsMember: context.userIsMember
+            };
 
-			$( faqtmpl(data) )
-				.find("dd")
-					.append( faq.render )
-					.end()
-				.appendTo(faqwrap);
+            $( context.faqtmpl(data) )
+                .find("dd")
+                    .append( faq.render )
+                    .end()
+                .appendTo(faqwrap);
 
-		});
-		return section;
-	}
+        });
+        return section;
+    };
 
-	$(function() {
-		faqtmpl = Handlebars.compile($("#faq-template").html());
-		var $main = $("#main");
+    FAQs.prototype.bindUIEvents = function() {
+        var context = this;
+        $( document ).bind("fetched", "#main", function() {
+            $("body").addClass("fetched");
+            context.renderBothSets();
+        });
+        context.$main.on("click", "dt", function(e) {
+            $(this)
+                .parent('dl')
+                    .toggleClass('closed');
+        });
+    };
 
-		function renderBothSets() {
-			$main.append(generateFAQSection('tiddlyspace'));
-            $main.append(generateFAQSection('tiddlywiki'));
-            $("#main").sieve({ itemSelector: "dl", textSelector: "dt", searchInput: $("#faqSearch") });
-		}
+    FAQs.prototype.renderBothSets = function() {
+        this.$main.append(this.generateFAQSection('tiddlyspace'));
+        this.$main.append(this.generateFAQSection('tiddlywiki'));
+        $("#main").sieve({ itemSelector: "dl", textSelector: "dt", searchInput: $("#faqSearch") });
+    };
 
-		if(fetched) {
-		    $("body").addClass("fetched");
-			renderBothSets();
-		} else {
-			$( document ).bind("fetched", "#main", function() {
-			    $("body").addClass("fetched");
-				renderBothSets();
-			});
-		}
+    exports.FAQs = new FAQs();
 
-		$main.on("click", "dt", function(e) {
-			var $this = $(this);
-			$this
-			    .parent('dl')
-			        .toggleClass('closed');
-		});
-
-		
-	});
-}(jQuery));
+}(window, jQuery));
